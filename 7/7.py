539b43ba8a833b0a410ceb94a5385cdd5e9e37f3
noname65 <57306016+noname65@users.noreply.github.com>
2022-03-21 19:19:00 -0400

    Update 7.py

--- 7/7.py
+++ 7/7.py
@@ -1,3 +1,9 @@
+#[Medium]This problem was asked by Facebook.
+#Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+#For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+#You can assume that the messages are decodable. For example, '001' is not allowed.
+
+#The process of decoding a message is similar to binary tree traversal.
 def function1(s):
     result=0
     if int(s[0])>0:
